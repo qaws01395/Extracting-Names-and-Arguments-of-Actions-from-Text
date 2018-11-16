f64aac5da6ae3e13a256e421ec2a76a3aad1acd4
Sue Chang <abc0963285350@gmail.com>
2018-11-16 11:49:18 -0600

    crawler folder

---
+++ crawler/crawler.py
@@ -0,0 +1,101 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+# import HTMLParser
+# import time
+import sys
+
+url = 'https://www.wikihow.com/Category:Furniture'
+resp = requests.get(url)
+soup = BeautifulSoup(resp.text, 'html.parser')
+# dcard_title = soup.find_all('h3', re.compile('PostEntry_title_'))
+dcard_title = soup.find_all('div','thumbnail s-height s-width')
+links = []
+for link in dcard_title:
+        link = 'https:' + [tag['href'] for tag in link.findAll('a',{'href':True})][0]
+        # print(link)
+        links.append(link)
+
+# create header
+# title  = "title" + "," + "category" + "," + "tel" + "," + "addr" + "," + "cost" + "," + "rank" + "," + "counts" + "," + "share" + "," + "collect"
+wikihow_list = open('wikihow_list_Furniture.txt','w')
+# write header
+# wikihow_list.write(title.encode('utf-8') + "\n")
+
+for i in range(len(links)):
+
+    res = requests.get(links[i])
+    print 'fecthing', links[i]
+    wikihow_list.write(('fecthing '+ links[i]).encode('utf-8') + "\n")
+
+    soup = BeautifulSoup(res.text.encode("utf-8"), 'html.parser')
+
+    header = soup.find('div',{'id':'intro'})
+    #
+    title = header.h1.a.string.strip()
+    # print('Subject: '+title)
+    wikihow_list.write(('Subject: '+title).encode('utf-8') + "\n")
+
+    methods = soup.find_all('div', re.compile('section steps'))
+    # methods_text = []
+    # print('method')
+    # print(methods[0])
+
+    for index, method in enumerate(methods):
+        # print 'Method', (index+1), method.find('span', {'class':'mw-headline'}).string
+        try:
+            method_header = method.find('span', {'class':'mw-headline'}).string
+            wikihow_list.write(('Method '+str(index+1)+' '+method_header).encode('utf-8') + "\n")
+        except Exception as e:
+            method_header = ""
+        # exception
+        # try:
+        #     method_header = method.find('span', {'class':'mw-headline'}).string
+        # except Exception as e:
+        #     method_header = ""
+
+        step_contents_arr = method.find_all('li', 'hasimage')
+        for step in step_contents_arr:
+            try:
+                step_num = step.find('div', {'class':'step_num'}).string
+                # print 'step', step_num
+                wikihow_list.write(('Step '+ step_num).encode('utf-8') + "\n")
+            except Exception as e:
+                step_num = ""
+            try:
+                step_content = step.find('div', {'class':'step'})
+                description = step_content.text
+                # main_step = step_content.find('b','whb').string
+                # print main_step
+                # print description
+                wikihow_list.write(description.encode('utf-8') + "\n")
+                # try:
+                #     notes_arr = step_content.find_all('li')
+                #     for i, note in enumerate(notes_arr):
+                #         print (i+1), note.string
+                # except Exception as e1:
+                #     notes_arr = ""
+
+            except Exception as e:
+                step_content = ""
+
+    # concatenate
+    # result = title + "," + category + "," + tel + "," + addr + "," + cost + "," + rank + "," + counts + "," + share + "," + collect
+    # wikihow_list.write(result.encode('utf-8') + "\n")
+
+    # sleep
+    # time.sleep(randint(1,5))
+    # clear_output()
+    print i
+
+    sys.stdout.flush()
+wikihow_list.write(('total subjects: '+str(i)).encode('utf-8') + "\n")
+# print 'total', i
+wikihow_list.close()
+
+
+# print('Crawled:')
+# print(dcard_title[0].text)
+# for index, item in enumerate(dcard_title[:10]):
+    # print("{0:2d}. {1}".format(index + 1, item.text.strip()))
+    # print(item.text.strip())
