a96e9b46ac0c7cf9e442a233d59794f4f96dd296
qaws01395 <qaws01395@github.com>
2018-11-17 16:32:01 -0600

    mark data

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # NLP-extraction-and-planning
+
+## Data processing
+
+### Manually mark data
+1. Run `python replacePrint.py` -> temp.txt (can be deleted), cookingTutorialCrawlEdit.csv are created
+2. Use editor open cookingTutorialCrawlEdit.csv, delete unnecessary text before 'Step 1: ...' and save as stage1_CT.csv
+3. Use tool (MS Office, Libre Office) to open stage1_CT.csv
+4. Replace action verbs with #, action arguments with @, save as stage2_CT.csv
+5. Delete all the words except for # and @, save as mark_CT.csv
--- replacePrint.py
+++ replacePrint.py
@@ -6,17 +6,54 @@
 
 
 
+# # Read in the file
+# file = open('cookingTutorialCrawl.txt', 'r')
+# filedata = file.read()
+# # print(filedata)
+#
+# filedata = ' '.join(filedata.split())
+# # print(filedata)
+#
+# # Replace the target string
+# filedata = filedata.replace(' ', ',').replace('.', '\n')
+# print(filedata)
+#
+# # Write the file out again
+# with open('cookingTutorialCrawlEdit.csv', 'w') as file:
+#     file.write(filedata)
+
+
 # Read in the file
-file = open('cookingTutorialCrawl.csv', 'r')
-filedata = file.read()
+file = open('cookingTutorialCrawl.txt', 'r')
+# filedata = file.read()
+# print(filedata)
+newFile = open('temp.txt', 'w')
+
+for line in file:
+    if "–" not in line and "Ingredients" not in line :
+        line = ' '.join(line.split())
+        line = line.replace(".", '\n').lstrip()
+        line = line.replace(',', '~')
+        # print(line)
+        newFile.write(line)
+newFile.close()
+file.close()
+
+newFile2 = open('cookingTutorialCrawlEdit.csv', 'w')
+file2 = open('temp.txt', 'r')
+for line in file2:
+    line = line.lstrip().replace(' ', ',')
+    # print(line)
+    newFile2.write(line)
+file2.close()
+newFile2.close()
+
 # print(filedata)
 
 # Replace the target string
-filedata = filedata.replace(' ', ',').replace('.', '\n')
-print(filedata)
+# filedata = filedata.replace(' ', ',').replace('.', '\n')
+# print(filedata)
 
 # Write the file out again
-with open('cookingTutorialCrawlEdit.csv', 'w') as file:
-    file.write(filedata)
-
-
+# with open('cookingTutorialCrawlEdit.csv', 'w') as file:
+#     file.write(filedata)
